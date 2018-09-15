1a5a9ef1be9b17c712232025047487449906ac55
Joyce98 <tamiranjeri98@gmail.com>
2018-09-15 12:15:37 +0300

    new Files

---
+++ ReactNative101/App.js
@@ -0,0 +1,23 @@
+import React,{ Component } from 'react';
+import { View, Text, Image ,StyleSheet} from 'react-native';
+import LoginForm from './src/components/LoginForm'
+
+export default class App extends Component{
+  render(){
+   
+    return (
+      <View style = { styles.container}>
+      <LoginForm/>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+  },
+
+});
